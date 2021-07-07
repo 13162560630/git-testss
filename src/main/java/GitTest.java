70292e6ccbb71b4f785451b5c637550fcbf52710
13162560630 <13162560630@163.com>
2021-07-07 16:55:59 +0800

    push commit3

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hot_fix test33!!!");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("pull22 test");
     }
 }
