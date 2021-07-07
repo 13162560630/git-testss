7743fe71441242320f62db520e51d36bf90b16cc
13162560630 <13162560630@163.com>
2021-07-07 15:45:55 +0800

    master commit3

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -4,6 +4,9 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git4");
+        System.out.println("master test11!!!");
+        System.out.println("master test22!!!");
+        System.out.println("master test33!!!");
         System.out.println("hot_fix test11!!!");
         System.out.println("hot_fix test22!!");
         System.out.println("hot_fix test33!!!");
